b220f87552c73af9010d1f7fd14ca759c2608fe1
starsarray <945467963@qq.com>
2026-04-26 18:24:25 +0800

    增加图标

--- app/src/main/java/com/example/scratchoff/MainActivity.kt
+++ app/src/main/java/com/example/scratchoff/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.scratchoff
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.scratchoff.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
